d1ab87936a1c44cf026a7f42bf14c3b28d7bca7d
Salman Ahmed <salmanahmedx@hotmail.com>
2022-05-18 11:37:27 +0600

    DELETE Operation

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,22 +1,35 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import getProducts from '../hooks/products';
+
 const Products = () => {
-    const [products] = getProducts();
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/products`)
+            .then(res => res.json())
+            .then(data => {
+                const valid = data.map(d => d.slots && d)
+                const prod = [];
+                valid.map(v => typeof v !== "undefined" && prod.push(v))
+                setProducts(prod);
+            })
+    }, [])
+
     return (
         <div>
             <h1 className="text-5xl pb-5">Products</h1>
             <div className="card-container grid grid-cols-3 gap-5 place-items-center">
                 {
-                    products.map(product => <div className="card w-96 bg-base-100 shadow-xl" key={product._id}>
+                    products?.map(product => <div className="card w-96 bg-base-100 shadow-xl" key={product?._id} >
                         <div className="card-body">
-                            <h2 className="card-title m-2 mx-auto">{product.name}</h2>
+                            <h2 className="card-title m-2 mx-auto">{product?.name}</h2>
                             <select name='time'>
                                 {
-                                    product.slots.map((s, index) => <option key={index}>{s}</option>)
+                                    product?.slots.map((s, index) => <option key={index}>{s}</option>)
                                 }
                             </select>
                             <div className="card-actions justify-center m-3">
-                                <Link to={`/products/${product._id}`}>
+                                <Link to={`/products/${product?._id}`}>
                                     <button className="btn btn-sm">Details</button>
                                 </Link>
                             </div>
--- src/pages/products/productsDetails.jsx
+++ src/pages/products/productsDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
-const ProductsDetails = () => {
+
+const ProductsDetails = ({ setProducts, products }) => {
     const { id } = useParams();
 
     //fetch product details - findOne
@@ -15,11 +16,12 @@ const ProductsDetails = () => {
 
     //update details
 
+
     const handleUpdate = (e) => {
+        const url = `http://localhost:5000/products/${id}`
         e.preventDefault()
         const inputValue = e.target.name.value;
         const updateDetails = { name: inputValue };
-        const url = `http://localhost:5000/products/${id}`
         fetch(url, {
             method: "PUT",
             headers: {
@@ -38,6 +40,25 @@ const ProductsDetails = () => {
             })
     }
 
+    //Delete Details
+
+    const handleDelete = id => {
+        const proceed = window.confirm(`Are you sure you want to delete?`)
+        if (proceed) {
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then(async res => {
+                    const data = await res.json();
+                    console.log(data)
+                    const remainingItems = products.filter(product => product._id !== Number(id))
+                    setProducts(remainingItems)
+                })
+
+        }
+    }
+
     return (
         <div className='text-xl'>
             <h1>ID: {itemDetails?._id}</h1>
@@ -50,7 +71,7 @@ const ProductsDetails = () => {
             <form onSubmit={handleUpdate}>
                 <input type="text" name="name" placeholder='Treatment Name' className='m-4' /><br></br>
                 <input type="submit" value="update" className='btn' />
-                <button className='delete btn m-2'>Delete</button>
+                <button className='delete btn m-2' onClick={() => handleDelete(id)}>Delete</button>
             </form>
         </div>
     );
